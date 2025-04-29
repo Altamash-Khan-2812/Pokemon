d0c9afdfef9bcb8acb7a98954e07f8c097620aeb
Altamash-Khan-2812 <altamashkhn281@gmail.com>
2025-04-29 18:47:40 +0530

    Initial Commit

---
+++ src/Pokemon.jsx
@@ -0,0 +1,82 @@
+import { useEffect } from "react";
+import "./index.css";
+import { useState } from "react";
+import { PokemonCard } from "./PokemonCard";
+
+export const Pokemon = () => {
+  const [pokemon, setPokemon] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [search, setSearch] = useState("");
+  const API = "https://pokeapi.co/api/v2/pokemon?limit=100";
+
+  const fetchPokemon = async () => {
+    try {
+      const res = await fetch(API);
+      const data = await res.json();
+
+      const detailedPokemonData = data.results.map(async (currPokemon) => {
+        const res = await fetch(currPokemon.url);
+        const data = await res.json();
+        return data;
+      });
+
+      const detailedResponse = await Promise.all(detailedPokemonData);
+
+      setPokemon(detailedResponse);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+      setError(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPokemon();
+  }, []);
+
+  const searchData = pokemon.filter((cuurPokemon) => {
+    return cuurPokemon.name.toLowerCase().includes(search.toLowerCase());
+  });
+
+  if (loading) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div>
+        <h1>{error.message}</h1>
+      </div>
+    );
+  }
+  return (
+    <section className="container">
+      <header>
+        <h1>Lets Catch Pokemon</h1>
+      </header>
+      <div className="pokemon-search">
+        <input
+          type="text"
+          placeholder="search pokemon"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+      <div>
+        <ul className="cards">
+          {searchData.map((currPokemon) => {
+            return (
+              <PokemonCard key={currPokemon.id} pokemonData={currPokemon} />
+            );
+          })}
+        </ul>
+      </div>
+    </section>
+  );
+};
